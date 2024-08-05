086196bdee7c42efa7dbbb0f3ef8d069d41b3764
Elihei2 <eheidari@student.ethz.ch>
2024-08-05 11:33:37 +0200

    added training

---
+++ src/segger/models/segger_model.py
@@ -0,0 +1,53 @@
+import torch
+from torch_geometric.nn import GATv2Conv, Linear, to_hetero
+from torch import Tensor
+from torch_geometric.typing import EdgeIndex
+
+class Segger(torch.nn.Module):
+    def __init__(self, init_emb: int = 16, hidden_channels: int = 32, out_channels: int = 32, heads: int = 3):
+        """
+        Initializes the Segger model.
+
+        Args:
+            init_emb (int): Initial embedding size.
+            hidden_channels (int): Number of hidden channels.
+            out_channels (int): Number of output channels.
+            heads (int): Number of attention heads.
+        """
+        super().__init__()
+        self.lin0 = Linear(-1, init_emb, bias=False)
+        self.conv_first = GATv2Conv((-1, -1), hidden_channels, heads=heads, add_self_loops=False)
+        self.lin_first = Linear(-1, hidden_channels * heads)
+        self.conv_last = GATv2Conv((-1, -1), out_channels, heads=heads, add_self_loops=False)
+        self.lin_last = Linear(-1, out_channels * heads)
+        
+    def forward(self, x: Tensor, edge_index: EdgeIndex) -> Tensor:
+        """
+        Forward pass for the Segger model.
+
+        Args:
+            x (Tensor): Node features.
+            edge_index (EdgeIndex): Edge indices.
+
+        Returns:
+            Tensor: Output node embeddings.
+        """
+        x = self.lin0(x)
+        x = x.relu()
+        x = self.conv_first(x, edge_index) + self.lin_first(x)
+        x = x.relu()
+        x = self.conv_last(x, edge_index) + self.lin_last(x)
+        return x
+    
+    def decode(self, z: Tensor, edge_label_index: EdgeIndex) -> Tensor:
+        """
+        Decode the node embeddings to predict edge values.
+
+        Args:
+            z (Tensor): Node embeddings.
+            edge_label_index (EdgeIndex): Edge label indices.
+
+        Returns:
+            Tensor: Predicted edge values.
+        """
+        return (z[edge_label_index[0]] * z[edge_label_index[1]]).sum(dim=-1)
---
+++ src/segger/training/train.py
@@ -0,0 +1,80 @@
+import os
+import torch
+import torch.nn.functional as F
+import torchmetrics
+from torchmetrics import F1Score
+import lightning as L
+from torch_geometric.loader import DataLoader
+from segger.models.segger_model import Segger
+from segger.data.utils import XeniumDataset
+from typing import Any, List
+
+class LitSegger(L.LightningModule):
+    def __init__(self, model: Segger):
+        """
+        Initializes the LitSegger module for training and validation.
+
+        Args:
+            model (Segger): The Segger model.
+        """
+        super().__init__()
+        self.model = model
+        self.validation_step_outputs = []
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+        
+    def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
+        """
+        Defines the training step.
+
+        Args:
+            batch (Any): The batch of data.
+            batch_idx (int): The index of the batch.
+
+        Returns:
+            torch.Tensor: The loss value.
+        """
+        z = self.model(batch.x_dict, batch.edge_index_dict)
+        output = torch.matmul(z['tx'], z['nc'].t()) 
+        edge_label_index = batch['tx', 'belongs', 'nc'].edge_label_index
+        out_values = output[edge_label_index[0], edge_label_index[1]]
+        edge_label = batch['tx', 'belongs', 'nc'].edge_label
+        loss = self.criterion(out_values, edge_label)
+        self.log("train_loss", loss, prog_bar=False, batch_size=batch.num_graphs)
+        return loss
+    
+    def validation_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
+        """
+        Defines the validation step.
+
+        Args:
+            batch (Any): The batch of data.
+            batch_idx (int): The index of the batch.
+
+        Returns:
+            torch.Tensor: The loss value.
+        """
+        z = self.model(batch.x_dict, batch.edge_index_dict)
+        output = torch.matmul(z['tx'], z['nc'].t()) 
+        edge_label_index = batch['tx', 'belongs', 'nc'].edge_label_index
+        out_values = output[edge_label_index[0], edge_label_index[1]]
+        edge_label = batch['tx', 'belongs', 'nc'].edge_label
+        loss = self.criterion(out_values, edge_label)
+        out_values = F.sigmoid(out_values)
+        auroc = torchmetrics.AUROC(task="binary")
+        auroc_res = auroc(out_values, edge_label)
+        f1 = F1Score(task='binary', num_classes=2).to(self.device)
+        f1_res = f1(out_values, edge_label)
+        self.log("validation_loss", loss, batch_size=batch.num_graphs)
+        self.log("validation_auroc",  auroc_res, prog_bar=False, batch_size=batch.num_graphs)
+        self.log("validation_f1",  f1_res, prog_bar=False, batch_size=batch.num_graphs)
+        return loss
+    
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        """
+        Configures the optimizer.
+
+        Returns:
+            torch.optim.Optimizer: The optimizer.
+        """
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
