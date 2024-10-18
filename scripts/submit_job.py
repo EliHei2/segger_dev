289a81ab46dbaa144948dada0f2f3070bbffa3e6
DÁNIEL UNYI <daniel.unyi.42@gmail.com>
2024-10-18 02:22:15 +0200

    Job submission with pipeline parameters

--- scripts/submit_job.py
+++ scripts/submit_job.py
@@ -1,13 +1,17 @@
 import yaml
 import subprocess
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", default="config.yaml", help="Path to the configuration YAML file")
 args = parser.parse_args()
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+config_file_path = os.path.join(script_dir, args.config)
+
 # Load the YAML configuration file
-with open(args.config, "r") as file:
+with open(config_file_path, "r") as file:
     config = yaml.safe_load(file)
 
 # Function to get Singularity command if enabled
@@ -28,25 +32,52 @@ def get_singularity_command(use_gpu=False):
 def get_python_command():
     return ["python3", "-m", "debugpy", "--listen", "0.0.0.0:5678", "--wait-for-client"] if config.get('use_debugpy', False) else ["python3"]
 
+# Function to get the correct base directory (local or container)
+def get_base_dir():
+    if config.get('use_singularity', False):
+        return config['path_mappings']['container_dir']
+    return config['path_mappings']['local_repo_dir']
+
+base_dir = get_base_dir()
+
 # Define the pipeline functions
 
 # Run the data processing pipeline
 def run_data_processing():
     command = get_singularity_command(use_gpu=False) + get_python_command() + [
-        "src/segger/cli/create_dataset_fast.py",
+        f"{base_dir}/src/segger/cli/create_dataset_fast.py",
         "--base_dir",
         config["preprocessing"]["base_dir"],
         "--data_dir",
         config["preprocessing"]["data_dir"],
         "--sample_type",
         config["preprocessing"]["sample_type"],
-        "--tile_width",
-        str(config["preprocessing"]["tile_width"]),
-        "--tile_height",
-        str(config["preprocessing"]["tile_height"]),
+        "--k_bd",
+        str(config["preprocessing"]["k_bd"]),
+        "--dist_bd",
+        str(config["preprocessing"]["dist_bd"]),
+        "--k_tx",
+        str(config["preprocessing"]["k_tx"]),
+        "--dist_tx",
+        str(config["preprocessing"]["dist_tx"]),
+        "--neg_sampling_ratio",
+        str(config["preprocessing"]["neg_sampling_ratio"]),
+        "--frac",
+        str(config["preprocessing"]["frac"]),
+        "--val_prob",
+        str(config["preprocessing"]["val_prob"]),
+        "--test_prob",
+        str(config["preprocessing"]["test_prob"]),
         "--n_workers",
-        str(config["preprocessing"]["workers"]),
+        str(config["preprocessing"]["n_workers"]),
     ]
+
+    if config["preprocessing"].get("tile_size") is not None:
+        command.extend(["--tile_size", str(config["preprocessing"]["tile_size"])])
+    if config["preprocessing"].get("tile_width") is not None:
+        command.extend(["--tile_width", str(config["preprocessing"]["tile_width"])])
+    if config["preprocessing"].get("tile_height") is not None:
+        command.extend(["--tile_height", str(config["preprocessing"]["tile_height"])])
 
     if config.get('use_lsf', False):
         command = [
@@ -56,7 +87,7 @@ def run_data_processing():
             "-o",
             config["preprocessing"]["output_log"],
             "-n",
-            str(config["preprocessing"]["workers"]),
+            str(config["preprocessing"]["n_workers"]),
             "-R",
             f"rusage[mem={config['preprocessing']['memory']}]",
             "-q",
@@ -64,6 +95,7 @@ def run_data_processing():
         ] + command
 
     try:
+        print(f"Running command: {command}")
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running data processing pipeline: {e}")
@@ -72,17 +104,39 @@ def run_data_processing():
 # Run the training pipeline
 def run_training():
     command = get_singularity_command(use_gpu=True) + get_python_command() + [
-        "src/segger/cli/train_model.py",
+        f"{base_dir}/src/segger/cli/train_model.py",
         "--dataset_dir",
         config["training"]["dataset_dir"],
         "--models_dir",
         config["training"]["models_dir"],
         "--sample_tag",
         config["training"]["sample_tag"],
+        "--init_emb",
+        str(config["training"]["init_emb"]),
+        "--hidden_channels",
+        str(config["training"]["hidden_channels"]),
+        "--num_tx_tokens",
+        str(config["training"]["num_tx_tokens"]),
+        "--out_channels",
+        str(config["training"]["out_channels"]),
+        "--heads",
+        str(config["training"]["heads"]),
+        "--num_mid_layers",
+        str(config["training"]["num_mid_layers"]),
+        "--batch_size",
+        str(config["training"]["batch_size"]),
         "--num_workers",
-        str(config["training"]["workers"]),
+        str(config["training"]["num_workers"]),
+        "--accelerator",
+        config["training"]["accelerator"],
+        "--max_epochs",
+        str(config["training"]["max_epochs"]),
         "--devices",
-        str(config["training"]["gpus"]),
+        str(config["training"]["devices"]),
+        "--strategy",
+        config["training"]["strategy"],
+        "--precision",
+        config["training"]["precision"],
     ]
 
     if config.get('use_lsf', False):
@@ -95,7 +149,7 @@ def run_training():
             "-o",
             config["training"]["output_log"],
             "-n",
-            str(config["training"]["workers"]),
+            str(config["training"]["num_workers"]),
             "-R",
             f"rusage[mem={config['training']['memory']}]",
             "-R",
@@ -107,6 +161,7 @@ def run_training():
         ] + command
 
     try:
+        print(f"Running command: {command}")
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running training pipeline: {e}")
@@ -115,7 +170,7 @@ def run_training():
 # Run the prediction pipeline
 def run_prediction():
     command = get_singularity_command(use_gpu=True) + get_python_command() + [
-        "src/segger/cli/predict.py",
+        f"{base_dir}/src/segger/cli/predict.py",
         "--segger_data_dir",
         config["prediction"]["segger_data_dir"],
         "--models_dir",
@@ -124,10 +179,32 @@ def run_prediction():
         config["prediction"]["benchmarks_dir"],
         "--transcripts_file",
         config["prediction"]["transcripts_file"],
+        "--batch_size",
+        str(config["prediction"]["batch_size"]),
+        "--num_workers",
+        str(config["prediction"]["num_workers"]),
+        "--model_version",
+        str(config["prediction"]["model_version"]),
+        "--save_tag",
+        config["prediction"]["save_tag"],
+        "--min_transcripts",
+        str(config["prediction"]["min_transcripts"]),
+        "--cell_id_col",
+        str(config["prediction"]["cell_id_col"]),
+        "--use_cc",
+        str(config["prediction"]["use_cc"]),
         "--knn_method",
         config["prediction"]["knn_method"],
-        "--num_workers",
-        str(config["prediction"]["workers"]),
+        "--file_format",
+        config["prediction"]["file_format"],
+        "--k_bd",
+        str(config["preprocessing"]["k_bd"]),
+        "--dist_bd",
+        str(config["preprocessing"]["dist_bd"]),
+        "--k_tx",
+        str(config["preprocessing"]["k_tx"]),
+        "--dist_tx",
+        str(config["preprocessing"]["dist_tx"]),
     ]
 
     if config.get('use_lsf', False):
@@ -140,7 +217,7 @@ def run_prediction():
             "-o",
             config["prediction"]["output_log"],
             "-n",
-            str(config["prediction"]["workers"]),
+            str(config["prediction"]["num_workers"]),
             "-R",
             f"rusage[mem={config['prediction']['memory']}]",
             "-R",
@@ -152,6 +229,7 @@ def run_prediction():
         ] + command
 
     try:
+        print(f"Running command: {command}")
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running prediction pipeline: {e}")
